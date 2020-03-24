031554f249419784791ba3ff7cfa7f1cba9e59ba
zeroEighteen <60764315+zeroEighteen@users.noreply.github.com>
2020-03-25 07:32:15 +0800

    added animation for mobile menu

--- homepageJs.js
+++ homepageJs.js
@@ -1,16 +1,22 @@
 const navBarList = document.querySelector("#navBarLinks");
 const mobileMenu = document.querySelector(".mobileHamburger");
-let hamburgerBool = true;
+let hamburgerBool = false;
+navBarList.classList.add("animated", "fadeInUp");
+navBarList.classList.add("navBarHidden");
+mobileMenu.classList.remove('menuOpen');
 function toggleMobileNavBar() {
     if (hamburgerBool == false) {
-        navBarList.classList.add("animated", "fadeInUp");
+        navBarList.classList.remove("animated", "fadeOutDown");
         navBarList.classList.remove("navBarHidden");
+        navBarList.classList.add("animated", "fadeInUp");
         mobileMenu.classList.add("menuOpen");
         hamburgerBool = true;
     } else {
-        navBarList.classList.add("animated", "fadeInUp");
-        navBarList.classList.add("navBarHidden");
+        navBarList.classList.remove("animated", "fadeInUp");
+        navBarList.classList.add("animated", "fadeOutDown");
         mobileMenu.classList.remove('menuOpen');
+        setTimeout(() => {  navBarList.classList.add("navBarHidden"); }, 700);
+
         hamburgerBool = false;
     }
 }
