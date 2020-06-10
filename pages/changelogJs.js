28a41dc1d7e99bda6f505b9cf309b301b29038ac
zeroEighteen <koh_yi_lei_elliot@s2019.ssts.edu.sg>
2020-06-11 06:43:34 +0800

    add final changes before project revision

--- pages/changelogJs.js
+++ pages/changelogJs.js
@@ -22,21 +22,3 @@ const toggleMobileNavBar = () => {
 		}, 350);
 	}
 };
-/*function toggleMobileNavBar() {
-  if (hamburgerBool == false) {
-    navBarList.classList.remove("animated", "fadeOutDown");
-    navBarList.classList.remove("navBarHidden");
-    navBarList.classList.add("animated", "fadeInUp");
-    mobileMenu.classList.add("menuOpen");
-    hamburgerBool = true;
-  } else {
-    navBarList.classList.remove("animated", "fadeInUp");
-    navBarList.classList.add("animated", "fadeOutDown");
-    mobileMenu.classList.remove("menuOpen");
-    setTimeout(() => {
-      navBarList.classList.add("navBarHidden");
-      hamburgerBool = false;
-    }, 350);
-  }
-}
-*/
